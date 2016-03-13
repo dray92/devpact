6edc5ecf5e592b2db13db0c83105981a10c916ca
Debosmit Ray <dray92@uw.edu>
2016-03-13 05:47:43 -0700

    no message

---
+++ app/src/main/java/com/example/devpact/DisplayImageActivity.java
@@ -0,0 +1,15 @@
+package com.example.devpact;
+
+import android.os.Bundle;
+import android.app.Activity;
+import com.example.devpact.R;
+
+public class DisplayImageActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_display_image);
+    }
+
+}
